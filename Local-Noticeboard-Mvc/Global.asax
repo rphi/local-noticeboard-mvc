682848388ac17039fe120c7c8a4d9630223c1e0e
Robert Phipps <rphippsy@gmail.com>
2015-08-16 09:17:35 +0100

    Created VS2013 project
    (hence all of the random stuff)

---
+++ Local-Noticeboard-Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Local_Noticeboard_Mvc.MvcApplication" Language="C#" %>
---
+++ Local-Noticeboard-Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Local_Noticeboard_Mvc.Startup))]
+namespace Local_Noticeboard_Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
