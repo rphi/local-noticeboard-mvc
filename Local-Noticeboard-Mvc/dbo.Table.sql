28d795f25fabb9ba2b66492960fe37a2b63f0b67
Robert Phipps <rphippsy@gmail.com>
2015-08-17 09:13:30 +0100

    Started work on the actual dashboard

---
+++ Local-Noticeboard-Mvc/dbo.Table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserId] INT NOT NULL, 
+    [Category] TEXT NULL, 
+    [Title] TEXT NULL, 
+    [Body] TEXT NULL, 
+    [Timestamp] TIMESTAMP NOT NULL DEFAULT GETDATE(), 
+    [Ranking] INT NOT NULL DEFAULT 0
+)
